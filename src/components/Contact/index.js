072ae893ee521cb286e3ae27fce8c8f638a3ef30
bonobo <arburpee@gmail.com>
2022-03-16 21:11:42 -0700

    added submit functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
 
 function ContactForm() {
+    const [formState, setFormState] = useState({name: '', email:'', message:''});
+    const {name, email, message } = formState;
+    
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    
     return (
         <section>
             <h1>Contact me</h1>
-            <form id='contact-form'>
-                // name input
+            <form id='contact-form' onSubmit={handleSubmit} >
+
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' />
+                    <input type='text' name='name' onChange={handleChange} defaultValue={name}/>
                 </div>
-                // email input
+
                 <div>
                     <label htmlFor='email'>Email address:</label>
-                    <input type='email' name='email' />
+                    <input type='email' name='email' onChange={handleChange} defaultValue={email}/>
                 </div>
-                // message text area
+
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows='5' />
+                    <textarea name='message' rows='5' onChange={handleChange} defaultValue={message}/>
                 </div>
                 <button type='submit'>Submit</button>
             </form>
