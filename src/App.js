26b63c7479fb5e8cdcf5e6e2aedce6d1127d844f
bonobo <arburpee@gmail.com>
2022-03-16 17:46:25 -0700

    use state and useeffect

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import About from './components/About'
+import Nav from './components/Nav'
+import Gallery from './components/Gallery'
+
+function App() {
+  const [categories] = useState([
+    {
+        name: 'commercial',
+        description: 'Photos of grocery stores, food strucks, and other commercial products'
+    },
+    {
+        name: 'portraits', 
+        description: 'Portraits of people in my life'
+    },
+    {
+        name: 'food',
+        description: 'Delicious delicacies'
+    },
+    {
+        name: 'landscape',
+        description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    }
+]);
+const [currentCategory, setCurrentCategory ] = useState(categories[0]);
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}>
+      </Nav>
+      <main>
+        <Gallery></Gallery>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
